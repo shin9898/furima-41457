dfd74fbd51f921c7f9f4ba608162713476e754d7
shin9898 <shintech9898@gmail.com>
2024-10-20 14:44:13 +0900

    Userモデルの単体テスト実行

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_10_14_085357) do
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
