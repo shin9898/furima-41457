03ea1b149c94811a164c3bcdaa71ddfc9c86ed7e
shin9898 <shintech9898@gmail.com>
2024-10-26 15:36:37 +0900

    商品データの保存と販売利益・販売手数料の自動表示、itemモデルの単体テスト実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,21 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-
   belongs_to :category
   belongs_to :status
   belongs_to :shipping_fee
   belongs_to :prefecture
   belongs_to :delivery_time
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+
+  validates :category_id, numericality: { other_than: 1, only_integer: true, message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1, only_integer: true, message: "can't be blank" }
+  validates :shipping_fee_id, numericality: { other_than: 1, only_integer: true, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, only_integer: true, message: "can't be blank" }
+  validates :delivery_time_id, numericality: { other_than: 1, only_integer: true, message: "can't be blank" }
+  
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
