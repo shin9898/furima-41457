7f6eee7579579a442130c74959404cfefc49a432
shin9898 <shintech9898@gmail.com>
2024-11-04 14:39:53 +0900

    Formオブジェクトを作成

---
+++ app/models/purchase_form.rb
@@ -0,0 +1,15 @@
+class PurchaseForm
+  include ActiveModel::Model
+
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'must be 10 or 11 digits long' }
+  end
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+end
